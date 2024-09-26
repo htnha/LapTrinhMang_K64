67630ed04ceabe6ef9ed1f7ec368d4fb04cec755
htnha <hoang.tuan.nha@gmail.com>
2024-09-26 07:58:58 +0700

    playwright

--- get_google.py
+++ get_google.py
@@ -3,8 +3,11 @@ from playwright.sync_api import sync_playwright
 def run(playwright):
     browser = playwright.chromium.launch(headless=False)  # Set headless=True for headless mode
     page = browser.new_page()
-    page.goto("https://www.google.com")
-    print(page.title())  # Prints the title of the page
+    page.goto("https://imgflip.com/memegenerator/Distracted-Boyfriend", timeout=0)  # Wait until the network is idle
+    title = page.query_selector("h1").inner_text()
+    image = page.query_selector("img.mm-img").get_attribute("src")
+
+    print(f"{title}: https://imgflip.com{image}")
     browser.close()
 
 with sync_playwright() as playwright:
